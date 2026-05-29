07e5f40780d89eb8cd4370696fa5bb9ec01bc4eb
Litoxam <litobernier@gmail.com>
2026-05-29 15:02:17 +0200

    fix: added list in front of DictReader

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -10,9 +10,15 @@ def convert_csv_to_json(filename):
     """Open csv file and then write it into json file"""
     try:
         with open(filename, mode='r') as csvfile:
-            csv_data = csv.DictReader(csvfile)
+            csv_data = list(csv.DictReader(csvfile))
         with open("data.json", 'w', encoding="utf-8") as jsonfile:
             json.dump(csv_data, jsonfile)
         return True
-    except Exception:
+    except FileNotFoundError:
         return False
+
+
+if __name__ == "__main__":
+    csv_file = "data.csv"
+    convert_csv_to_json(csv_file)
+    print(f"Data from {csv_file} has been converted to data.json")
