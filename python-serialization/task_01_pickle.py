ac69fcaf7c5c2adb2678a06f48dc3bcb24555d3c
Litoxam <litobernier@gmail.com>
2026-05-29 14:24:15 +0200

    Pickling custom classes

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Pickling Custom Classes"""
+
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """print the content of the object"""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """serialization"""
+        with open(filename, "wb") as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """deserialization"""
+        with open(filename, "rb") as file:
+            return pickle.load(file)
+
+if __name__ == "__main__":
+    # Create an instance of CustomObject
+    obj = CustomObject(name="Lito", age=29, is_student=True)
+    print("Original Object:")
+    obj.display()
+
+    # Serialize the object
+    obj.serialize("object.pkl")
+
+    # Deserialize the object into a new instance
+    new_obj = CustomObject.deserialize("object.pkl")
+    print("\nDeserialized Object:")
+    new_obj.display()
+    
