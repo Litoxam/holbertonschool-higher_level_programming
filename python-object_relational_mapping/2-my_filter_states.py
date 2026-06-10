2b12534370115505a83b54f55cdf34058ddfb10c
Litoxam <litobernier@gmail.com>
2026-06-10 19:35:49 +0200

    fix: add of BINARY in the SQL query

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     cursor = connexion.cursor()
 
     # SQL query execution
-    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+    cursor.execute("SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC"
                    .format(sys.argv[4]))
 
     # get the result
