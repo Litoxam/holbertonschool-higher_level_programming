f1fed0b390233584c7cc1716386ec93e038f0006
Litoxam <litobernier@gmail.com>
2026-06-10 11:50:50 +0200

    fix: docstrings

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-python file that contains the class definition of a State
-and an instance Base = declarative_base()
+Contains the State class and Base object.
 """
 
 from sqlalchemy import Column, Integer, String
@@ -11,6 +10,9 @@ Base = declarative_base()
 
 
 class State(Base):
+    """State class."""
+
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
