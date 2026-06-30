d6084655aceff58f65802f8a7191cca978ddb922
Litoxam <litobernier@gmail.com>
2026-06-30 14:13:59 +0200

    feat: Parses an array and display its data

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const array = [ // stocks data into an array
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+let i = 0;
+while (i < array.length) { // parse the array with an index
+  console.log(array[i]);
+  i++;
+}
