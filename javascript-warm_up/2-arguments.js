1f81174143c66c0208237e7720cc287164874768
Litoxam <litobernier@gmail.com>
2026-06-30 12:42:35 +0200

    feat: prints a message depending of the number of arguments passed

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,3 +1,13 @@
-const { argv } = require('node:process');
+#!/usr/bin/node
 
-console.log(process.arv[1]);
+const args = process.argv; // array of arguments
+
+if (args.length === 2) {
+  console.log('No argument');
+} else {
+  if (args.length === 3) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
+}
