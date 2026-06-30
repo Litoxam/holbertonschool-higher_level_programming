c9d2bc8f6228fac8bed72fefd453cb6f11012941
Litoxam <litobernier@gmail.com>
2026-06-30 12:34:20 +0200

    fix: semistandard

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,5 @@
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+#!/usr/bin/node
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
