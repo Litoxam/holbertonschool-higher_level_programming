eb1b2d0ec0720454c2c07fe7121520b06231ea61
Litoxam <litobernier@gmail.com>
2026-06-30 12:54:27 +0200

    fix: concatenate

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,8 +2,10 @@
 
 const args = process.argv;
 
-if (args[2]) {
-  console.log('c is ' + args[2]);
-} else {
-  console.log('c is undefined');
+if (args[2] && args[3]) {
+  console.log(args[2] +  ' is ' + args[3]);
+} else if (args[2] && !args[3]){
+  console.log(args[2] + ' is undefined');
+} else{
+  console.log('undefined is undefined');
 }
