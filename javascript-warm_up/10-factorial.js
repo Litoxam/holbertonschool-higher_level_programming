9adeb66c332176589ebec56d6e87a71b6de2182b
Litoxam <litobernier@gmail.com>
2026-06-30 14:56:05 +0200

    feat: prints the factorial of argv2, using recursion

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const x = parseInt(args[2]);
+
+function factorial (n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(x));
