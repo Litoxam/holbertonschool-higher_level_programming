35247ff30abe6f09e8aa8fc48a529ec1ef522085
Litoxam <litobernier@gmail.com>
2026-06-30 14:58:31 +0200

    fix: returns 1 if argv2 is not a number

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -4,7 +4,7 @@ const args = process.argv;
 const x = parseInt(args[2]);
 
 function factorial (n) {
-  if (n === 0 || n === 1) {
+  if (n === 0 || n === 1 || isNaN(n)) {
     return 1;
   }
   return n * factorial(n - 1);
