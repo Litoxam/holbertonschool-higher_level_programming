d9beddc099ede2f546e30ac870a99818ecccee6b
Litoxam <litobernier@gmail.com>
2026-06-30 12:55:34 +0200

    fix: semistandard

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -3,9 +3,9 @@
 const args = process.argv;
 
 if (args[2] && args[3]) {
-  console.log(args[2] +  ' is ' + args[3]);
-} else if (args[2] && !args[3]){
+  console.log(args[2] + ' is ' + args[3]);
+} else if (args[2] && !args[3]) {
   console.log(args[2] + ' is undefined');
-} else{
+} else {
   console.log('undefined is undefined');
 }
