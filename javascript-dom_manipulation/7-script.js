44725b462e9338d8076846a6501ca78fc05f2ad9
Litoxam <litobernier@gmail.com>
2026-07-03 14:41:13 +0200

    feat: list every movies titles of a json file in a html list

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const listMovies = document.getElementById('list_movies');
+
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then(function (response) {
+    return response.json(); // GET all the data
+  })
+  .then(function (data) {
+    for (let i = 0; i < data.results.length; i++) {
+      const newFilm = document.createElement('li');
+      newFilm.textContent = data.results[i].title;
+      listMovies.appendChild(newFilm);
+    }
+  });
+  
