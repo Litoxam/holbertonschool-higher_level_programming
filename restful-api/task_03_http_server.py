63b25b0fa002ef080d047ac44b192e2d600e43fc
Litoxam <litobernier@gmail.com>
2026-07-07 10:59:29 +0200

    feat: Create basic HTTP server with API endpoints

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,54 @@
+import http.server
+import json
+
+class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
+    """Simple HTTP API handler."""
+
+    def do_GET(self):
+        """GET request"""
+
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write("Hello, this is a simple API!".encode())
+
+        elif self.path == "/data":
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write("OK".encode())
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write("OK".encode())
+
+        elif self.path == "/info":
+            info = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+            }
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write("OK".encode())
+        
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write("Endpoint not found".encode())
+
+
+if __name__ == "__main__":
+    server_address = ("127.0.0.1", 8000)
+    httpd = http.server.HTTPServer(server_address, SimpleAPIHandler)
+    print("Server running on http://127.0.0.1:8000")
+    httpd.serve_forever()
